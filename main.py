546c093ff605694bc17cb15bc3c44f3be9a3a86c
Tony <antoniogomezgallardo@gmail.com>
2022-07-17 00:13:39 +0200

    adding create and load password file

--- main.py
+++ main.py
@@ -14,10 +14,28 @@ class PasswordManager(object):
 		with open(path, 'wb') as f:
 			f.write(self.key)
 
+	
 	def load_key(self, path):
+		with open(path, 'rb') as f:
+			self.key = f.read()
+
+
+	def create_password_file(self, path, initial_values=None)
+		self.password_file = path
+
+		if initial_values is not None:
+			pass # TODO: add password function
+
+
+	def load_password_file(self, path):
+		self.password_file = path
 
 		with open(path, 'rb') as f:
-			self.key = f.read
+			for line in f:
+				site, encrypted = line.split(":")
+				self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+
+
 
 
 pm = PasswordManager()
